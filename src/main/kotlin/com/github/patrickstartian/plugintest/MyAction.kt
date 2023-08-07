92b4cac39b9a9290c443c16deeddfef9b5083820
GitHub Action <action@github.com>
2023-08-07 11:26:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/patrickstartian/plugintest/MyAction.kt
+++ src/main/kotlin/com/github/patrickstartian/plugintest/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.patrickstartian.plugintest
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/patrickstartian/plugintest/MyBundle.kt
+++ src/main/kotlin/com/github/patrickstartian/plugintest/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.patrickstartian.plugintest
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
